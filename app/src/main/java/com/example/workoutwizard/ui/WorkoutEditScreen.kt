e449d8a7f912292847e9050abc63e179669b3c81
Erik Reisswig <erikreisswig@gmx.de>
2024-06-30 18:29:48 +0200

    Workout Bearbeiten Informationen - Workout Name und Datum wird angezeigt

--- app/src/main/java/com/example/workoutwizard/ui/WorkoutEditScreen.kt
+++ app/src/main/java/com/example/workoutwizard/ui/WorkoutEditScreen.kt
@@ -1,12 +1,23 @@
 package com.example.workoutwizard.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import com.example.workoutwizard.R
+import com.example.workoutwizard.helper.overviewDateString
+import com.example.workoutwizard.ui.components.MainSpacer
+import com.example.workoutwizard.ui.components.NavigationHeader
 import com.example.workoutwizard.ui.model.WorkoutViewModel
+import java.time.LocalDate
 
 @Composable
 fun WorkoutEditScreen(
@@ -24,8 +35,36 @@ fun WorkoutEditScreen(
         Column(
             modifier = modifier
         ) {
-            Text(text = selectedWorkout.data.name)
-            Text(text = workoutID)
+            NavigationHeader(
+                headerText = selectedWorkout.data.title
+            )
+            MainSpacer()
+            WorkoutEditDate(
+                workoutDate = selectedWorkout.createdAt 
+            )
         }
+    }
+}
+
+@Composable
+fun WorkoutEditDate(
+    modifier: Modifier = Modifier,
+    workoutDate: LocalDate
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "${stringResource(id = R.string.workout_edit_planned)}:",
+            modifier = Modifier
+                .padding(
+                    end = dimensionResource(id = R.dimen.same_content_space)
+                )
+        )
+        Text(
+            text = overviewDateString(workoutDate),
+            fontWeight = FontWeight.Bold
+        )
     }
 }
